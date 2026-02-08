9ead9059c8d1feb1e193ff4f2a89770ec64cbbad
Adithya <adithyab710@gmail.com>
2026-02-08 13:29:02 +0530

    parantheses

---
+++ src/main/java/com/algos/otherQns/paranthesesString.java
@@ -0,0 +1,43 @@
+package com.algos.otherQns;
+
+import java.util.Stack;
+
+public class paranthesesString {
+    public int minParanthesesToRemoveToMakeItValid(String s){
+        Stack<Character> stack = new Stack<>();
+        //lee(t)cod(e))
+
+        char[] charArray = s.toCharArray();
+        for(char c: charArray){
+            if(!Character.isAlphabetic(c)){
+                switch (c) {
+                    case '(' -> stack.add(c);
+                    case ')' -> {
+                        if (stack.peek().equals('('))
+                            stack.pop();
+                        else stack.push(c);
+                    }
+                }
+            }
+        }
+        return stack.size();
+    }
+    public int minRemove(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.size();
+    }
+
+}
